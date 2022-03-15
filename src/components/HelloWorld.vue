fd01b4fe64bbec38ca9bd53c64e0c2f2392690d2
sergeymitrichev <sergeymitrichev@yandex.ru>
2022-03-15 15:51:32 +0300

    chp

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,54 +1,48 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { ref } from 'vue'
 import JsSIP from 'jssip'
 
 defineProps<{ msg: string }>()
 
-const socket = new JsSIP.WebSocketInterface('')
+const socket = new JsSIP.WebSocketInterface('wss://webclientrtudev.satel.org:5059')
 const ua = new JsSIP.UA({
-  uri: '',
-  authorization_user: '',
-  password: '',
+  uri: 'sip:4494$ROOT@webclientrtudev.satel.org:5059',
+  authorization_user: 'Bj7s9H6G',
+  password: 'Gp3t9V0W',
   sockets: [socket],
-  register: true,
+  register: false,
   register_expires: 120,
   realm: 'debian'
 })
 
-ua.start()
-ua.register()
+const registered = ref(false)
+const connected = ref(false)
+const loading = ref(false)
 
-const count = ref(0)
-const registered = computed(() => ua.isRegistered())
-const connected = computed(() => ua.isConnected())
-const status = computed(() => ua.status)
+ua.addListener('registered', () => registered.value = ua.isRegistered())
+ua.addListener('unregistered', () => registered.value = ua.isRegistered())
+ua.addListener('registrationFailed', () => registered.value = ua.isRegistered())
+ua.addListener('connected', () => connected.value = ua.isConnected())
+ua.addListener('disconnected', () => connected.value = ua.isConnected())
+
+const connect = () => {
+  loading.value = true
+  ua.start()
+  loading.value = false
+}
+
+const register = () => {
+  loading.value = true
+  ua.register()
+  loading.value = false
+}
 </script>
 
 <template>
-  <h1>{{ registered }} | {{ connected }} | {{ status }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <h1>Connected: {{ connected }} | Registered: {{ registered }}</h1>
+  <button @click="connect" :disabled="loading || connected">Connect</button>
+  <span style="padding: 0 10px"></span>
+  <button @click="register" :disabled="loading || registered">Register</button>
 </template>
 
 <style scoped>
